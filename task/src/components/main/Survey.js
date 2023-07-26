683912b07ef85c82453036460ea2c49a53c42645
ishwar4230 <atcikss9@kgpian.iitkgp.ac.in>
2023-07-26 21:58:05 +0530

    added location suggestion

--- task/src/components/main/Survey.js
+++ task/src/components/main/Survey.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import "./surStyle.css";
 import Page2 from "../page2/Page2"
@@ -15,8 +16,12 @@ const Survey = () => {
 
     const [purpose, SetPurpose] = useState("");
     const [origin, setOrigin] = useState("");
+    const [originsugg, setoriginsugg] = useState([]);
+    const originref = useRef(null);
 
     const [destn, SetDest] = useState("");
+    const [destsugg, setdestsugg] = useState([]);
+    const destref = useRef(null);
 
     const [timeb, setTimeb] = useState("");
 
@@ -34,6 +39,107 @@ const Survey = () => {
         navigate("/page2", { state: data });
     };
 
+    const handleoriginchange = async (event) => {
+        const orgval = event.target.value;
+        setOrigin(orgval);
+        document.getElementById("origin-suggestion-div").style.display="block";
+        // Make an API call to OpenCage Data Geocoding API
+        const API_KEY = 'd3efb0eb93ac4011b071c4454be325bc';
+        const apiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
+            orgval
+        )}&key=${API_KEY}&limit=5`; // You can change the limit as needed
+
+        try {
+            const response = await axios.get(apiUrl);
+            const results = response.data.results;
+            // Extract location suggestions from the API response
+            const origin_suggestion = results.map((result) => result.formatted);
+            setoriginsugg(origin_suggestion);
+        } catch (error) {
+            console.error('Error fetching location suggestions:', error);
+            setoriginsugg([]); // Clear suggestions on error
+        }
+        if(originsugg.length==0)
+      {
+       document.getElementById("origin-suggestion-div").style.display="none";
+      }
+
+    }
+    const handleoriginsuggclick = (suggestion) => {
+        setOrigin(suggestion);
+        setoriginsugg([]); // Clear suggestions after selecting one
+        document.getElementById("origin-suggestion-div").style.display="none";
+      };
+
+      useEffect(() => {
+        // Add a click event listener to the document
+        const handleClickOutsideorigin = (event) => {
+          if (originref.current && !originref.current.contains(event.target)) {
+            setoriginsugg([]); // Clear suggestions when clicking outside the textbox
+            document.getElementById("origin-suggestion-div").style.display="none";
+
+          }
+        };
+    
+        document.addEventListener('click', handleClickOutsideorigin);
+    
+        return () => {
+          // Clean up the event listener when the component unmounts
+          document.removeEventListener('click', handleClickOutsideorigin);
+        };
+      }, []);
+
+      const handledestchange = async (event) => {
+        const destval = event.target.value;
+        SetDest(destval);
+        document.getElementById("destination-suggestion-div").style.display="block";
+        // Make an API call to OpenCage Data Geocoding API
+        const API_KEY = 'd3efb0eb93ac4011b071c4454be325bc';
+        const apiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
+            destval
+        )}&key=${API_KEY}&limit=5`; // You can change the limit as needed
+
+        try {
+            const response = await axios.get(apiUrl);
+            const results = response.data.results;
+            // Extract location suggestions from the API response
+            const dest_suggestion = results.map((result) => result.formatted);
+            setdestsugg(dest_suggestion);
+        } catch (error) {
+            console.error('Error fetching location suggestions:', error);
+            setdestsugg([]); // Clear suggestions on error
+        }
+        if(destsugg.length==0)
+      {
+       document.getElementById("destination-suggestion-div").style.display="none";
+      }
+
+    }
+    const handledestsuggclick = (suggestion) => {
+        SetDest(suggestion);
+        setdestsugg([]); // Clear suggestions after selecting one
+        document.getElementById("destination-suggestion-div").style.display="none";
+      };
+
+      useEffect(() => {
+        // Add a click event listener to the document
+        const handleClickOutsidedest = (event) => {
+          if (destref.current && !destref.current.contains(event.target)) {
+            setdestsugg([]); // Clear suggestions when clicking outside the textbox
+            document.getElementById("destination-suggestion-div").style.display="none";
+
+          }
+        };
+    
+        
+        document.addEventListener('click', handleClickOutsidedest);
+    
+        return () => {
+          // Clean up the event listener when the component unmounts
+          document.removeEventListener('click', handleClickOutsidedest);
+        };
+      }, []);
+      
     const collectdata = async () => {
         //console.log(origin,destn,timeb,traveltime);
         let result = await fetch("http://localhost:5000/register", {
@@ -73,7 +179,7 @@ const Survey = () => {
             <div className='purpose total'>
                 <p>The Purpose of Your last/daily trip reported here</p>
                 <select name='purpose' className='drop-down' onChange={(e) => SetPurpose(e.target.value)}>
-                <option value="">select</option>
+                    <option value="">select</option>
                     <option value="work">work</option>
                     <option value="Education">Education</option>
                     <option value="Shopping">Shopping</option>
@@ -81,16 +187,35 @@ const Survey = () => {
                     <option value="Other">Other</option>
 
                 </select>
-              
+
             </div>
 
             <div className='origin total'>
                 <p>State your trip origin </p>
-                <input type="text" value={origin} onChange={(e) => setOrigin(e.target.value)} name="origin" className='textb' />
+                <input type="text" value={origin} onChange={handleoriginchange} name="origin" className='textb' />
+            </div>
+            <div id='origin-suggestion-div' ref={originref}>
+            <ul>
+        {originsugg.map((suggestion, index) => (
+          <li key={index} className='suggested-place' onClick={() => handleoriginsuggclick(suggestion)}>
+            {suggestion}
+          </li>
+        ))}
+      </ul>
             </div>
             <div className='destination total'>
                 <p>State your trip destination </p>
-                <input type="text" value={destn} onChange={(e) => SetDest(e.target.value)} name="destn" className='textb' />
+                <input type="text" value={destn} onChange={handledestchange} name="destn" className='textb' />
+            </div>
+
+            <div id='destination-suggestion-div' ref={destref}>
+            <ul>
+        {destsugg.map((suggestion, index) => (
+          <li key={index} className='suggested-place' onClick={() => handledestsuggclick(suggestion)}>
+            {suggestion}
+          </li>
+        ))}
+      </ul>
             </div>
             <div className='boardtime total'>
                 <p>Time of boarding </p>
@@ -111,11 +236,11 @@ const Survey = () => {
             <div className='samemode total'>
                 <p>Same travel mode throughout the trip</p>
                 <select name='samemode' className='drop-down' onChange={(e) => Setmode(e.target.value)}>
-                <option value="">select</option>
+                    <option value="">select</option>
                     <option value="Yes">Yes</option>
                     <option value="No">No</option>
                     <option value="Maybe">Maybe</option>
-                    
+
 
                 </select>
             </div>
@@ -140,7 +265,7 @@ const Survey = () => {
             <div className='level total'>
                 <p>Level of crowding</p>
                 <select name='crowd-level' className='drop-down' onChange={(e) => Setcrowd(e.target.value)}>
-                <option value="">select</option>
+                    <option value="">select</option>
                     <option value="1">Level-1</option>
                     <option value="2">Level-2</option>
                     <option value="3">Level-3</option>
